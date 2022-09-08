d12e406aa0335c57a02dfec2a617a3e5ee0b250c
ayaadel887 <aya.abdelgoad@gmail.com>
2022-09-08 07:35:33 +0200

    creat/organize project folders

---
+++ src/component/Tour.js
@@ -0,0 +1,5 @@
+const Tour = () => {
+    return ( <h1> tour component</h1> );
+}
+ 
+export default Tour;
---
+++ src/component/Tours.js
@@ -0,0 +1,7 @@
+const Tours = () => {
+    return (<h2>
+        tours component
+    </h2>);
+}
+ 
+export default Tours;
